b4dd8ae282df0cd6ee08450fd477732f1f35afa8
skippynea <53440878+skippynea@users.noreply.github.com>
2023-08-21 11:29:36 +1100

    fix models/index.js with new(require(..)) line30

--- app.js
+++ app.js
@@ -36,10 +36,10 @@ app.use((req, res, next) => {
   });
 
 // path to the folder to store the "images"
-app.use ('/images', express.static(path.join(__dirname, 'images')));
+// app.use ('/images', express.static(path.join(__dirname, 'images')));
 
 // i need an "app.use" for the POST
-app.use('/api/messages', messagesRoutes);
-app.use('/api/auth', userRoutes);
+// app.use('/api/messages', messagesRoutes);
+// app.use('/api/auth', userRoutes);
 
 module.exports = app;
